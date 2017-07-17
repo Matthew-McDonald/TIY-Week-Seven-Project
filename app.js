d162179b0f535a78b7249c39f93aea24f4bd64f8
Matt McDonald <mattmcd1221@gmail.com>
2017-07-17 07:05:29 -0400

    add stat path

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const mustacheExpress = require('mustache-express');
-const passport = require('passport');
-const BasicStrategy = require('passport-http').BasicStrategy;
-const bcrypt = require('bcryptjs');
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mustacheExpress = require("mustache-express");
+const passport = require("passport");
+const BasicStrategy = require("passport-http").BasicStrategy;
+const bcrypt = require("bcryptjs");
 
 // const passport = require('passport');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost/stattracker');
+mongoose.connect("mongodb://localhost/stattracker");
 
-Activity = require('./models/activities');
-User = require('./models/user')
+Activity = require("./models/activities");
+User = require("./models/user");
 
-
-// Authentication Section DONT DELETE
+// Authentication Section DONT DELETE********************
 // example of modifying new password
 // var user = User.findOne({name: "mattmcd"}, function(err, user){
 //   user.password = 'test';
@@ -28,66 +26,110 @@ User = require('./models/user')
 //   })
 // });
 
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    User.findOne({ name: username }, function(err, user){
+passport.use(
+  new BasicStrategy(function(username, password, done) {
+    User.findOne({ name: username }, function(err, user) {
       console.log("User Check");
-      if (user && bcrypt.compareSync(password, user.password)){
+      if (user && bcrypt.compareSync(password, user.password)) {
         return done(null, user);
       }
       return done(null, false);
     });
-  }
-));
+  })
+);
 
 //api authentication for passport
-app.get('/api/auth',
-  passport.authenticate('basic', {session: false}), function (req, res) {
-      res.send('You have been authenticated, ' + req.user.name);
+app.get(
+  "/api/auth",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    res.send("You have been authenticated, " + req.user.name);
   }
 );
 
 //Tells the client to use the correct home route
-app.get('/', function(req, res){
-  res.send('Please use /api/...');
+app.get("/", function(req, res) {
+  res.send("Please use /api/...");
 });
 
 //Gets all the activities that are being tracked
-app.get('/api/activities', passport.authenticate('basic', {session: false}), function(req, res){
-  Activity.getActivities(function(err, activities){
-    if(err){
-      throw err;
-    }
-    res.json(activities);
-  });
-});
+app.get(
+  "/api/activities",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    Activity.getActivities(function(err, activities) {
+      if (err) {
+        throw err;
+      }
+      res.json(activities);
+    });
+  }
+);
 
 //Creates new activity
-app.post('/api/activities', passport.authenticate('basic', {session: false}), function(req, res){
-  var activity = req.body;
-  Activity.addActivity(activity, function(err, activity){
-    if(err){
-      throw err;
-    }
-    res.json(activity);
-  });
-});
+app.post(
+  "/api/activities",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    var activity = req.body;
+    Activity.addActivity(activity, function(err, activity) {
+      if (err) {
+        throw err;
+      }
+      res.json(activity);
+    });
+  }
+);
 
 //Show information about one id
-app.get('/api/activities/:_id', passport.authenticate('basic', {session: false}), function(req, res){
-  Activity.getActivityById(req.params._id, function(err, activity){
-    if(err){
-      throw err;
-    }
-    res.json(activity);
-  });
-});
+app.get(
+  "/api/activities/:_id",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    Activity.getActivityById(req.params._id, function(err, activity) {
+      if (err) {
+        throw err;
+      }
+      res.json(activity);
+    });
+  }
+);
 
 //Updates an activity by id
-app.put('/api/activities/:_id', passport.authenticate('basic', {session: false}), function(req, res){
-  var id = req.params._id
-  var activity = req.body;
-  Activity.updateActivity(id, activity, {}, function(err, activity){
+app.put(
+  "/api/activities/:_id",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    var id = req.params._id;
+    var activity = req.body;
+    Activity.updateActivity(id, activity, {}, function(err, activity) {
+      if (err) {
+        throw err;
+      }
+      res.json(activity);
+    });
+  }
+);
+
+//Deletes an activity by id
+app.delete(
+  "/api/activities/:_id",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    var id = req.params._id;
+    Activity.removeActivity(id, function(err, activity) {
+      if (err) {
+        throw err;
+      }
+      res.json(activity);
+    });
+  }
+);
+
+//Finds activity by name
+
+app.get('/api/activities/name/:name', passport.authenticate('basic', {session: false}), function(req, res){
+  Activity.getActivityByName(req.params.name, function(err, activity){
     if(err){
       throw err;
     }
@@ -95,19 +137,20 @@ app.put('/api/activities/:_id', passport.authenticate('basic', {session: false})
   });
 });
 
-//Deletes an activity by id
-app.delete('/api/activities/:_id', passport.authenticate('basic', {session: false}), function(req, res){
-  var id = req.params._id
-  Activity.removeActivity(id, function(err, activity){
+
+//Pulls activity by stat
+app.get('/api/activities/stat/:stat', passport.authenticate('basic', {session: false}), function(req, res){
+  Activity.getActivityByStat(req.params.stat, function(err, activity){
     if(err){
       throw err;
     }
     res.json(activity);
   });
 });
+
 
 //Listen for port 27017, must match the port mongod is using
 
-app.listen(27017, function(req, res){
+app.listen(27017, function(req, res) {
   console.log("Up and running!");
 });
--- models/activities.js
+++ models/activities.js
@@ -1,56 +1,56 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 //Activities schema
 var activitiesSchema = mongoose.Schema({
-      name:{
-        type:String,
-        required: true
-       },
-      metric:{
-        type:Number,
-        required: true
-       },
-      metric_value:{
-        type:String,
-        required: true
-       },
-      date:{
-        type:Date,
-        required: true
-       }
+  name: {
+    type: String,
+    required: true
+  },
+  metric: {
+    type: Number,
+    required: true
+  },
+  metric_value: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
 });
 
-var Activity = module.exports = mongoose.model('Activity', activitiesSchema);
+var Activity = (module.exports = mongoose.model("Activity", activitiesSchema));
 
 //Gets all activities
-module.exports.getActivities = function(callback, limit){
+module.exports.getActivities = function(callback, limit) {
   Activity.find(callback).limit(limit);
-}
+};
 
 // Get Activity by Id
-module.exports.getActivityById = function(id, callback){
+module.exports.getActivityById = function(id, callback) {
   Activity.findById(id, callback);
-}
+};
 
 // Add Actviity
-module.exports.addActivity = function(activity, callback){
+module.exports.addActivity = function(activity, callback) {
   Activity.create(activity, callback);
-}
+};
 
-// Update Actviity by id
-module.exports.updateActivity = function(id, activity, options, callback){
-  var query = {_id: id};
+// Update Actvivity by id
+module.exports.updateActivity = function(id, activity, options, callback) {
+  var query = { _id: id };
   var update = {
     name: activity.name,
     metric: activity.metric,
     metric_value: activity.metric_value,
     date: activity.date
-  }
+  };
   Activity.findOneAndUpdate(query, update, options, callback);
-}
+};
 
 // Delete Actviity by id
-module.exports.removeActivity = function(id, callback){
-  var query = {_id: id};
+module.exports.removeActivity = function(id, callback) {
+  var query = { _id: id };
   Activity.remove(query, callback);
-}
+};
